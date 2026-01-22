529e2e2c79b30e9131e6a9d50494eaeababa1c96
simone <simo.x@outlook.it>
2026-01-22 23:48:21 +0100

    Simple correction into App initializer

--- src/main/java/it/SimoSW/util/bootstrap/ApplicationInitializer.java
+++ src/main/java/it/SimoSW/util/bootstrap/ApplicationInitializer.java
@@ -7,17 +7,6 @@ import it.SimoSW.model.dao.database.*;
 public class ApplicationInitializer {
 
     /* =========================
-       DAO
-       ========================= */
-
-    private final ImageDAO imageDAO;
-    private final GalleryDAO galleryDAO;
-    private final UserDAO userDAO;
-    private final EngineDAO engineDAO;
-    private final CustomerDAO customerDAO;
-
-
-    /* =========================
        Controller applicativi
        ========================= */
 
@@ -30,11 +19,12 @@ public class ApplicationInitializer {
     public ApplicationInitializer() {
 
         /* ===== DAO concreti ===== */
-        this.imageDAO = new DatabaseImageDAO();
-        this.galleryDAO = new DatabaseGalleryDAO();
-        this.userDAO = new DatabaseUserDAO();
-        this.engineDAO = new DatabaseEngineDAO();
-        this.customerDAO = new DatabaseCustomerDAO();
+        ImageDAO imageDAO = new DatabaseImageDAO();
+        GalleryDAO galleryDAO = new DatabaseGalleryDAO();
+        UserDAO userDAO = new DatabaseUserDAO();
+        EngineDAO engineDAO = new DatabaseEngineDAO();
+        CustomerDAO customerDAO = new DatabaseCustomerDAO();
+
 
         /* ===== Controller ===== */
         this.galleryController = new EngineController(engineDAO, imageDAO);
