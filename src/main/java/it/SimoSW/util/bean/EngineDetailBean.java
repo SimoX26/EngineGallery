53b34214e0851ae14c27413d637855331f8d1b17
simone <simo.x@outlook.it>
2026-02-06 20:00:16 +0100

    Adding EngineDetailBean

--- src/main/java/it/SimoSW/util/bean/EngineDetailBean.java
+++ src/main/java/it/SimoSW/util/bean/EngineDetailBean.java
@@ -1,4 +1,25 @@
 package it.SimoSW.util.bean;
 
+import it.SimoSW.model.Engine;
+import it.SimoSW.model.Image;
+
+import java.util.List;
+
 public class EngineDetailBean {
+
+    private final Engine engine;
+    private final List<Image> images;
+
+    public EngineDetailBean(Engine engine, List<Image> images) {
+        this.engine = engine;
+        this.images = images;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
 }
