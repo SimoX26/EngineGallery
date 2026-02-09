bc9feaee9d026fbd7fdd4b391a47d0f30864d06e
simone <simo.x@outlook.it>
2026-02-09 22:53:04 +0100

    Simple commit

--- src/main/java/it/SimoSW/util/bootstrap/ApplicationInitializer.java
+++ src/main/java/it/SimoSW/util/bootstrap/ApplicationInitializer.java
@@ -15,7 +15,6 @@ public class ApplicationInitializer {
     private final AuthenticationController authenticationController;
     private final DashboardController dashboardController;
 
-
     public ApplicationInitializer() {
 
         /* ===== DAO concreti ===== */
